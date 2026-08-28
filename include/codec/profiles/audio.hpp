d04f6a71cb4137cb5556577e083431b9f62e2091
jordanlegare <jordanlegare3@gmail.com>
2026-08-28 02:49:49 -0400

    Stage C.6: include W0 statement compatibility surface

--- include/codec/profiles/audio.hpp
+++ include/codec/profiles/audio.hpp
@@ -2,14 +2,18 @@
 
 #include <codec/audio.hpp>
 #include <codec/inference.hpp>
+#include <codec/statement.hpp>
 #include <codec/watermark.hpp>
 
 namespace codec::profiles::audio {
 
 using ::codec::CarrierBand;
+using ::codec::FeedStatement;
 using ::codec::SeparationBackend;
 using ::codec::SeparationRequest;
 using ::codec::SeparationResult;
+using ::codec::StatementState;
+using ::codec::StatementVerification;
 using ::codec::WatermarkEmbedReport;
 using ::codec::WatermarkObservation;
 using ::codec::WatermarkPolicy;
@@ -19,5 +23,9 @@ using ::codec::carrier_band_name;
 using ::codec::default_separation_backend;
 using ::codec::detect_watermarks;
 using ::codec::embed_watermark;
+using ::codec::generate_ed25519_keypair;
+using ::codec::issue_statement;
+using ::codec::statement_state_name;
+using ::codec::verify_statement;
 
 }  // namespace codec::profiles::audio

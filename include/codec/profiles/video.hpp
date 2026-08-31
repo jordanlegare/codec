e449c6cef7fc8d37ec77b5578c2202b0d8297526
jordanlegare <jordanlegare3@gmail.com>
2026-08-30 20:32:45 -0400

    Declare verified H.1 PCM audio state API

--- include/codec/profiles/video.hpp
+++ include/codec/profiles/video.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <codec/archive.hpp>
+#include <codec/audio.hpp>
 
 #include <compare>
 #include <cstddef>
@@ -15,6 +16,7 @@ namespace codec::profiles::video {
 
 inline constexpr RecordTypeCode video_profile_descriptor_record_type = 0x0100;
 inline constexpr RecordTypeCode raw_video_frame_state_record_type = 0x0101;
+inline constexpr RecordTypeCode video_pcm16_audio_state_record_type = 0x0102;
 
 enum class PixelLayout : std::uint8_t {
   gray8 = 1,
@@ -105,6 +107,23 @@ struct VerifiedRawVideoFrame {
 
 Result<std::vector<VerifiedRawVideoFrame>> query_verified_raw_video_frames(
     const CodaArchive& archive, const VideoFrameQuery& query = {});
+
+struct VideoAudioQuery {
+  std::optional<StreamId> stream;
+  std::optional<RecordTimeRange> time;
+  std::size_t maximum_results{1024};
+  std::uint64_t maximum_encoded_bytes{1024ULL * 1024ULL * 1024ULL};
+};
+
+struct VerifiedVideoPcm16Audio {
+  Pcm16State state;
+  RecordInfo state_record;
+  std::vector<RecordInfo> source_records;
+  StreamProvenance provenance;
+};
+
+Result<std::vector<VerifiedVideoPcm16Audio>> query_verified_video_pcm16_audio(
+    const CodaArchive& archive, const VideoAudioQuery& query = {});
 
 struct FfmpegVideoIngestRequest {
   std::string source_uri;
